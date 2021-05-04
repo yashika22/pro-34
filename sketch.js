3c7b4667814d969f85c5b57c34c4002e80966671
yashika22 <78300824+yashika22@users.noreply.github.com>
2021-05-04 15:17:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ var database;
 var foodS,foodStock;
 
 function preload(){
-dogImg = loadImage("images/dogImg.png");
-dogHappy = loadImage("images/dogImg1.png");
+dogImg = loadImage("dogImg.png");
+dogHappy = loadImage("dogImg1.png");
 }
 
 function setup() {
